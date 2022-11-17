3d914fe1960d881d8d64b206f8cdfaf5bc2f25ad
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-11-17 17:54:20 -0500

    fore

--- game-of-thrones/src/Navbar.js
+++ game-of-thrones/src/Navbar.js
@@ -4,16 +4,12 @@ import {Link} from "react-router-dom"
 
 function NavBar () {
 
- 
+
  return (
-
+    // NavBar
       <div className="navBar">
-
       <Link to= "/components/Quote"> <h1 className="title"> Game Of Thrones</h1></Link> 
-
       <Link to='/components/AboutMe' className='AboutMe'><h1> AboutMe </h1></Link>
-
-
       </div>
     );
   };
