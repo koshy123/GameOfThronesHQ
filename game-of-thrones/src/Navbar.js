45335a5b865a998a83131ff5bc0e126de0a06285
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-11-17 17:58:47 -0500

    add

--- game-of-thrones/src/Navbar.js
+++ game-of-thrones/src/Navbar.js
@@ -6,7 +6,6 @@ function NavBar () {
 
 
  return (
-    // NavBar
       <div className="navBar">
       <Link to= "/components/Quote"> <h1 className="title"> Game Of Thrones</h1></Link> 
       <Link to='/components/AboutMe' className='AboutMe'><h1> AboutMe </h1></Link>
